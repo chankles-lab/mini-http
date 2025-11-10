6889e167f03df92a6bc83e36363c89b6bfe44797
chankles-lab <kxywdq6b6j@privaterelay.appleid.com>
2025-11-10 11:29:29 -0600

    listening server from beej's guid

--- server.c
+++ server.c
@@ -6,9 +6,30 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+#include <signal.h>
 
 #define PORT "1234"
 #define BACKLOG 5
+
+void sigchld_handler(int s) {
+    (void)s;
+
+    int saved_errno = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main() {
     int status;
@@ -17,88 +38,82 @@ int main() {
     struct addrinfo *p, *q; // iterates thru servinfo
     char ipstr[INET6_ADDRSTRLEN]; // buffer to hold the ip address string for printing
     int sockfd, new_fd; // file descriptor? will be used to return from socket call
-    int bind_status;
     struct sockaddr_storage their_addr;
     socklen_t addr_size;
+    int yes = 1;
+    struct sigaction sa;
 
     memset(&hints, 0, sizeof hints); // make sure struct 'hints' is empty
-    hints.ai_family = AF_UNSPEC; // don't care ip4 or ip6
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets, as opposed to datagram
     hints.ai_flags = AI_PASSIVE; // fill in my IP for me
 
-    if ((status = getaddrinfo(NULL, "1234", &hints, &servinfo)) != 0) {
-        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
-        exit(1);
+    if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 1;
     }
 
-    // servinfo now points to a linked list of 1 or more struct addrinfos
-
-    // ... do everything until you don't need servinfo anymore ... 
-    printf("IP addresses for localhost\n\n");
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        void *addr;
-        char *ipver;
-        struct sockaddr_in *ipv4;
-        struct sockaddr_in6 *ipv6;
-
-        // get the pointer to the address itself,
-        // different fields in IPv4 and IPv6:
-        if (p->ai_family == AF_INET) { // IPv4
-            ipv4 = (struct sockaddr_in *)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-            ipver = "IPv4";
-        } else { // IPv6
-            ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-            addr = &(ipv6->sin6_addr);
-            ipver = "IPv6";
-        }
-
-        // convert the IP to a string and print it:
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-        printf("  %s: %s\n", ipver, ipstr);
-    }
-
-    printf("Binding to socket\n");
-    for (q = servinfo; q !=NULL; q = q->ai_next) {
-        sockfd = socket(q->ai_family, q->ai_socktype, q->ai_protocol);
-        if (sockfd == -1) {
+    for (p = servinfo; p !=NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
 
-        bind_status = bind(sockfd, q->ai_addr, q->ai_addrlen);
-        if (bind_status == -1) {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
             perror("server: bind");
             continue;
         }
 
         break; // both calls successful
     }
-    printf("Successfully returned sockfd: %d\n", sockfd);
-    printf("Successfully binded to socket: %d\n", bind_status);
 
     freeaddrinfo(servinfo); // free the linked list
 
-    int listen_status = listen(sockfd, BACKLOG);
-    if (listen_status == -1) {
-        perror("server: listen");
+    assert(p != NULL);
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
         exit(1);
     }
 
-    addr_size = sizeof their_addr;
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-    close(sockfd);
-    if (new_fd == -1) {
-        perror("server: accept");
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if(sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
         exit(1);
     }
 
-    char *msg = "Hello...can you hear me?\n";
-    int len, bytes_sent;
+    printf("server: waiting for connections...\n");
 
-    len = strlen(msg);
-    bytes_sent = send(new_fd, msg, len, 0);
+    while(1) {
+        addr_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
 
-    close(new_fd);
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), ipstr, sizeof ipstr);
+        printf("server: got connection from %s\n", ipstr);
+
+        if (!fork()) {
+            close(sockfd);
+            char *msg = "Hello...can you hear me?\n";
+            int len = strlen(msg);
+            if (send(new_fd, msg, len, 0) == -1) {
+                perror("send");
+            }
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);
+    }
     return 0;
 }
